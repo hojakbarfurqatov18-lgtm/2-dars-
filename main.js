4a62dab89ac10c1b4b30887fc6a6ed0fcbdad6cd
hojakbarfurqatov18-lgtm <hojakbarfurqatov18@gmail.com>
2025-10-02 16:20:35 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+
+let ism = "name"
+
+prompt(`Salom ismingizni kiriting`)
+
+console.log(ism);
+
+let raqam = Number(`123456789`)
+
+prompt(`Nomeringizni yozing`)
+
+console.log(raqam);
+
+let togri = Boolean
+prompt(`Siz 16 yoshdan katta misiz`)
+
+console.log(togri);
+
+confirm(`Hamma malumotlarni tog'ri kiritganingizga ishonchingiz komilmi?`)
+
+console.log();
